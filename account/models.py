34ada899c7f425139e7cfb42d76c75d120e8d58b
mlyazzat <200103016@stu.sdu.edu.kz>
2022-03-03 22:42:05 +0600

    Update models.py

--- account/models.py
+++ account/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Investor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    birth_date  = models.DateTimeField
+    gender = models.CharField(max_length=1)
+    current_money = models.IntegerField
+
+class Startupper(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateTimeField
+    gender = models.CharField(max_length=1)
+
+
+
+
+
