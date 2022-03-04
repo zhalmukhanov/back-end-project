383ea511596cf470d23b559da9c6545ef29db3da
mlyazzat <200103016@stu.sdu.edu.kz>
2022-03-04 13:21:12 +0600

    startup model migrated

--- startup/models.py
+++ startup/models.py
@@ -1,3 +1,16 @@
+import account.models
+from account.models import *
 from django.db import models
 
-class
+from account import models as Startupper
+
+
+class Startup(models.Model):
+    starupper_id = models.ForeignKey(account.models.Startupper, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    date_uploaded = models.DateTimeField(auto_now_add=True)
+    initial_capital = models.IntegerField()
+    accumulated_capital = models.IntegerField()
+
+
