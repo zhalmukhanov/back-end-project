b3a908eaaf295b0fe4fffdece3c7734761082b7f
mlyazzat <200103016@stu.sdu.edu.kz>
2022-03-07 13:25:36 +0600

    models updated(categories added)

--- startup/models.py
+++ startup/models.py
@@ -6,16 +6,42 @@ from account import models as Startupper
 from account.models import Startupper
 
 class Startup(models.Model):
-    startupper_id = models.ForeignKey(Startupper, on_delete=models.CASCADE)
+    startupper = models.ForeignKey(Startupper, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     date_uploaded = models.DateTimeField(auto_now_add=True)
     initial_capital = models.IntegerField()
     accumulated_capital = models.IntegerField()
 
+    categories = [
+        ('IT', 'IT'),
+        ('Ecology', 'Ecology'),
+        ('Electronic', 'Electronic'),
+        ('Health', 'Health'),
+        ('Education', 'Education'),
+        ('Gaming', 'Gaming'),
+        ('Small Business', 'Small Business'),
+        ('Other', 'Other')
+    ]
+    category = models.CharField(max_length=20, choices=categories, default='Other')
+
+
+
     def percentage(self):
         return float('{:.1f}'.format(self.accumulated_capital * 100 / self.initial_capital))
 
     def s_name(self):
-        s = Startupper.objects.get(id = self.starupper_id_id)
+        s = Startupper.objects.get(id = self.startupper_id)
         return s.first_name + " " + s.last_name
+
+
+
+class Categories(models.Model):
+    IT = 'IT'
+    Ecology = 'ECO'
+    Electronic = 'ELEC'
+    Health = 'HH'
+    Education = 'ED'
+    Gaming = 'GG'
+    Business = 'BS'
+
